056b619faa418f3e2300637fa75a95043339b7a3
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-08 00:04:06 +0600

    problem solvign done

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let i = 1;
-while(i < 10){
-    i++
-    if(i==5){
-        break;
+// let i = 1;
+// while(i < 10){
+//     i++
+//     if(i==5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// 1–20 এর মধ্যে যেগুলো 3 দিয়ে ভাগ যায় সেগুলো print করো
+for (let i = 1; i <= 20; i++){
+    if(i % 3 == 0){
+        console.log(i);
     }
-    console.log(i);
 }
+// 3,6,9,12,15,18,
